0ec6edfab9f90ddb67ffe804bf0c632cd0b3a436
Parimi <nitupari@amazon.com>
2021-08-09 13:23:53 -0700

    on 9th

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> This is from Amplify demo on Aug 8th! </h1>
+        <h1> Hello from Amplify demo on Aug 9th! </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
